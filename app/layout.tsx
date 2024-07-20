14a0a401ef1206dfd8b6da7708182da48cef2f74
Sebastian Olguin <sebastian.olguin@datco.cl>
2024-07-19 20:55:57 -0400

    Creacion componente navbar.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={urbanist.className}>{children}</body>
+      <body className={urbanist.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
